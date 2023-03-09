e0a651280c2a2a2006d3a01b5bbcc31cf00aa478
HarisM <harismahjoob42@gmail.com>
2023-03-09 02:00:26 -0800

    added package json and required modules

--- src/templateHTML.js
+++ src/templateHTML.js
@@ -64,6 +64,70 @@ const renderTeam = (team) => {
     const html = [];
 
     //Add manager
-    html.push(team.filter((employee) => employee.getRole() === 'Manager').map((manager) => renderManager(manager)));
+    html.push(
+        team
+            .filter((employee) => employee.getRole() === 'Manager')
+            .map((manager) => renderManager(manager)));
 
-}
+    //Add engineers
+    html.push(
+        team
+            //Filter to only include engineers
+            .filter((employee) => employee.getRole() === 'Engineer')
+            .map((engineer) => renderEngineer(engineer))
+            .join('')
+    );
+
+    //Add interns
+    html.push(
+        team
+            //Filter to only include interns
+            .filter((employee) => employee.getRole() === 'Intern')
+            .map((intern) => renderIntern(intern))
+            .join('')
+    );
+
+    //Return complete HTML
+    return html.join('');
+};
+
+
+//Render main html
+const renderMain = (team) => `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profiles</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/dist/style.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="team-profile-header col-12 mb-3 team-profile jumbotron">
+                <h1 class="team-profile-title text-center">Team</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="team-mem-container col-12 d-flex flex-wrap justify-content-around">
+                ${renderTeam(team)}
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+`;
+
+module.exports = renderMain;
